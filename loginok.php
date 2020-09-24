1c5580a767730242487aa228d44237b8d9911ff0
Emiliano <elaneri@gmail.com>
2020-09-24 00:27:14 -0300

    test

--- loginok.php
+++ loginok.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once 'vendor/autoload.php';
-
 use Firebase\JWT\JWT;
 
 $jwt = $_GET["TOKEN"];
@@ -12,7 +11,7 @@ $key = getenv('PUBLIC_TOKEN_K');
 
 
 $data = JWT::decode($jwt, $key, array('HS256'));
-
+print_r($data);
 var_dump($data);
 
 
