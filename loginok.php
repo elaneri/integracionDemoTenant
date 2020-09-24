3155c493983106ba166e44d1df822023e5b24291
Emiliano <elaneri@gmail.com>
2020-09-24 00:22:02 -0300

    test

--- loginok.php
+++ loginok.php
@@ -1,31 +1,16 @@
 <?php
+require_once 'vendor/autoload.php';
 
+use Firebase\JWT\JWT;
 
-$recievedJwt = $_GET["TOKEN"];
-$secret_key = getenv('PUBLIC_TOKEN_K');
-
-
-
-// Split a string by '.' 
-$token = explode(".", $recievedJwt);
+$jwt = $_GET["TOKEN"];
+$key = getenv('PUBLIC_TOKEN_K');
 
 
 
+$data = JWT::decode($jwt, $key, array('HS256'));
 
-$header = json_decode(base64_decode(strtr($token[0],'-_','+/')),true);
-$signature = bin2hex(base64_decode(strtr($token[2],'-_','+/')));
-
-
-echo $token[0]." \n";
-echo $token[1]." \n";
-echo $token[2]." \n";
-echo $signature." \n";
-echo hash_hmac('sha512',"$token[0].$token[1]",$secret_key)." \n";
-
-
-if ($signature!=hash_hmac('sha512',"$token[0].$token[1]",$secret_key)) return false;
-
-
+var_dump($data);
 
 
 ?>
