0ed45c629dc6e8e01af718ca9edc73bbeaefd267
Emiliano <elaneri@gmail.com>
2020-09-24 08:09:41 -0300

    config

--- loginok.php
+++ loginok.php
@@ -15,9 +15,74 @@ $header = \Firebase\JWT\JWT::jsonDecode(Firebase\JWT\JWT::urlsafeB64Decode($head
 var_dump($header->alg);
 
 
-$data = JWT::decode($jwt, $key, array('HS512'));
-print_r($data);
-var_dump($data);
+$decoded = JWT::decode($jwt, $key, array('HS512'));
+$decoded_array = (array) $decoded;
+$userIfo = CallAPI("GET","https://ssoia.herokuapp.com/Usuarios/"+$decoded_array["client_id"]);
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    
+	$headers = array(
+    	'Content-type: application/json',
+    	'x-api-key: $key',
+	);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Login Test</h1>
+<p>
+	<?php
+
+	echo "TOKEN INFO:\n" . print_r($decoded_array, true) . "\n";
+
+	?>
+</p>
+<p>
+	<?php
+
+	echo "USER INFO :\n" . $userInfo . "\n";
+
+	?>
+</p>
+
+
+</body>
+</html> 
